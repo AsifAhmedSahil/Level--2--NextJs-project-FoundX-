50928e0b78bff41a5729728a5403b4720a7144c9
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-13 23:08:56 +0600

    Implementing user checks and role-based routes

--- FoundX-client/src/middleware.ts
+++ FoundX-client/src/middleware.ts
@@ -2,13 +2,39 @@
 /* eslint-disable prettier/prettier */
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+
+const AuthRoutes = ['/login','/register']
+
+const roleBasedRoutes = {
+    USER:[/^\/profile/],
+    ADMIN:[/^\/admin/],
+
+}
+
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+
+    const {pathname} =request.nextUrl
+
+    const user =undefined;
+    // const user = {
+    //     name:"asif"
+    // }
+
+    if(!user){
+        if(AuthRoutes.includes(pathname)){
+            return NextResponse.next()
+        } else {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
+    }
+
+
+  return NextResponse.redirect(new URL('/', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
+  matcher: ['/login','/register','/profile'],
 }
