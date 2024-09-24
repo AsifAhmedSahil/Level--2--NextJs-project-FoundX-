6f0455d8c963a0b664c27d8305845f4e63060568
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-24 21:59:37 +0600

    Handling the addition of dynamic form fields

--- FoundX-client/src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ FoundX-client/src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,16 +1,18 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/jsx-sort-props */
 /* eslint-disable prettier/prettier */
 "use client";
 
 import FXInput from "@/src/components/form/FXInput";
 import { Button } from "@nextui-org/button";
+import { Divider } from "@nextui-org/divider";
 import { FieldValues, FormProvider, SubmitHandler, useFieldArray, useForm } from "react-hook-form";
 
 export default function CreatePostPage() {
   const methods = useForm();
   const { control,handleSubmit } = methods;
 
-  const {} = useFieldArray({
+  const {fields ,append, remove} = useFieldArray({
     control,
     name: "questions",
   });
@@ -20,11 +22,33 @@ export default function CreatePostPage() {
 
   }
 
+  const handleFieldAppend = () =>{
+    append({ name:"questions" })
+  }
+
   return (
-    <div>
+    <div className="">
       <FormProvider {...methods}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <FXInput name="title" label="Title" />
+
+          <Divider className="my-5"/>
+
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Owner Verification Address</h1>
+            <Button onClick={()=>handleFieldAppend()}>Append</Button>
+          </div>
+          <Divider className="my-5"/>
+
+          {
+            fields.map((field,index)=>(
+              <div key={field.id} className="mb-3">
+
+              <FXInput  name={`questions.${index}.value`} label="Question"/>
+              </div>
+            ))
+          }
+
           <Button type="submit">Post</Button>
         </form>
       </FormProvider>
--- FoundX-client/src/app/(WithCommonLayout)/(user)/profile/layout.tsx
+++ FoundX-client/src/app/(WithCommonLayout)/(user)/profile/layout.tsx
@@ -9,12 +9,12 @@ import { ReactNode } from "react"
 const layout = ({children}:{children:ReactNode}) => {
   return (
     <Container>
-      <div className="w-full my-12 flex gap-12">
+      <div className="w-full my-12 flex gap-12 ">
         <div className="w-2/5">
           {/* sidebar */}
           <Sidebar/>
         </div>
-        <div className="3/5">
+        <div className="w-3/5 ">
         {children}
         </div>
 
