a84ff05218791b508db973b3be14c6537575c73d
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-24 12:27:31 +0600

    Setting up the form for the create post page

---
+++ FoundX-client/src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/jsx-sort-props */
+/* eslint-disable prettier/prettier */
+"use client";
+
+import FXInput from "@/src/components/form/FXInput";
+import { Button } from "@nextui-org/button";
+import { FieldValues, FormProvider, SubmitHandler, useFieldArray, useForm } from "react-hook-form";
+
+export default function CreatePostPage() {
+  const methods = useForm();
+  const { control,handleSubmit } = methods;
+
+  const {} = useFieldArray({
+    control,
+    name: "questions",
+  });
+
+  const onSubmit : SubmitHandler<FieldValues>= (data) =>{
+    console.log(data)
+
+  }
+
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FXInput name="title" label="Title" />
+          <Button type="submit">Post</Button>
+        </form>
+      </FormProvider>
+    </div>
+  );
+}
